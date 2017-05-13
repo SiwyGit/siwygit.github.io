6a784611c0306a1fc775770eff93ebe241bd1e73
SiwyGit <pavel.sadowski@gmail.com>
2017-05-13 22:57:43 +0200

    added ngRoute

--- controller.js
+++ controller.js
@@ -1,7 +1,22 @@
-var app = angular.module('divApp', []);
+var app = angular.module('divApp', ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "table.html"
+    })
+    .when("/company/:companyID", {
+        templateUrl : "company.html",
+        controller : "compCtrl"
+    });
+});
 app.controller('divCtrl', function ($scope, $http) {
     $http.get('datatable1.json').success(function (data) {
         $scope.dividends = data.dividends2017;
     });
-        
+});
+app.controller('compCtrl', function ($scope, $routeParams, $http) {
+    $http.get('datatable1.json').success(function (data) {
+        $scope.dividend = data.dividends2017[$routeParams.companyID];
+    });
+
 });
